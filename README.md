8288fbe7f454ac01ebcfb5bc9029299a0a6a3692
Nelson Chan <chakflying@hotmail.com>
2019-09-27 04:48:50 -0400

    Updated working receive message

---
+++ README.md
@@ -0,0 +1,9 @@
+# CS3700 Project 2 - BGP Router
+
+## High Level Approach
+
+## Challenges faced
+
+## Testing
+
+## External Libraries Used
--- src/bin/router.rs
+++ src/bin/router.rs
@@ -1,19 +1,34 @@
 use clap::{App, Arg};
 use ipnet::IpSub;
-use std::io::{BufRead, BufReader};
+use serde::{Deserialize, Serialize};
+use serde_json::{Result, Value};
 use std::net::Ipv4Addr;
-use std::os::unix::net::{UnixListener, UnixStream};
-use std::thread;
 use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+use unix_socket::{UnixSeqpacket, UnixSeqpacketListener};
+
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+extern crate unix_socket;
 
 #[derive(Copy, Clone)]
 enum NeighborType {
     Cust,
     Peer,
     Prov,
+}
+
+#[derive(Debug)]
+enum BGPPacketType {
+    Update,
+    Revoke,
+    Data,
+    NoRoute,
+    Dump,
+    Table,
+    Unknown,
 }
 
 struct InitNeighbor {
@@ -24,7 +39,15 @@ struct InitNeighbor {
 struct Neighbor {
     ip: Ipv4Addr,
     n_type: NeighborType,
-    listener: UnixListener,
+    stream: UnixSeqpacket,
+}
+
+#[derive(Debug)]
+struct BGPPacket {
+    src: Ipv4Addr,
+    dst: Ipv4Addr,
+    p_type: BGPPacketType,
+    msg: Value,
 }
 
 impl InitNeighbor {
@@ -32,33 +55,55 @@ impl InitNeighbor {
         Neighbor {
             ip: self.ip,
             n_type: self.n_type,
-            listener: UnixListener::bind(self.ip.to_string())
-                .expect(&format!("Erorr binding to Unix socket {}.", self.ip)),
+            stream: UnixSeqpacket::connect(self.ip.to_string())
+                .expect(&format!("Error connecting to Unix socket {}.", self.ip)),
         }
     }
 }
 
 impl Neighbor {
+    fn set_timeout(&self) {
+        self.stream
+            .set_read_timeout(Some(Duration::new(1, 0)))
+            .expect("Error setting read timeout.");
+        self.stream
+            .set_write_timeout(Some(Duration::new(1, 0)))
+            .expect("Error setting write timeout.");
+    }
+
     fn listen(&self, tx: mpsc::Sender<String>) {
-        for stream in self.listener.incoming() {
-            match stream {
-                Ok(stream) => {
-                    self.handle_message(stream, tx.clone());
-                }
-                Err(err) => {
-                    error!("Error when receiving message from socket: {}", err);
+        loop {
+            if let Ok(Some(err)) = self.stream.take_error() {
+                println!("Got error: {:?}", err);
+                break;
+            }
+            let mut buf = [0; 4096];
+            let mut totalcount = 0;
+            loop {
+                let count = match self.stream.recv(&mut buf) {
+                    Ok(c) => {
+                        totalcount += c;
+                        c
+                    }
+                    Err(_err) => {
+                        // info!("[{}] Recv error: {}",self.ip, err);
+                        0
+                    }
+                };
+                if count == 0 {
                     break;
                 }
             }
-        }
-    }
-
-    fn handle_message(&self, stream: UnixStream, tx: mpsc::Sender<String>) {
-        let stream = BufReader::new(stream);
-        for line in stream.lines() {
-            let output = line.unwrap();
-            debug!("got message: {}", &output);
-            tx.send(output).expect("Error sending message back to main thread.");
+            let s = String::from_utf8_lossy(&buf).to_string();
+            if totalcount == 0 {
+                continue;
+            }
+            debug!(
+                "[{}] Got message with length {}: {}",
+                self.ip, totalcount, s
+            );
+            tx.send(s[..totalcount].to_string())
+                .expect("Error sending message back to main thread.");
         }
     }
 }
@@ -103,14 +148,51 @@ fn main() {
             .connect(),
         );
     }
+    let mut threads = Vec::new();
     for nei in neighbors {
         let new_tx = tx.clone();
-        thread::spawn(move || {
+        threads.push(thread::spawn(move || {
+            nei.set_timeout();
+            debug!("Spawning new thread for Neighbor {}", nei.ip);
             nei.listen(new_tx);
-        });
+        }));
     }
 
     for received in rx {
-        println!("Got: {}", received);
+        debug!("[Main Thread] Got packet with length {}", received.len());
+        let p: Value = match serde_json::from_str(&received[..]) {
+            Ok(p) => p,
+            Err(e) => {
+                error!("Error parsing received packet: {}", e);
+                Value::default()
+            }
+        };
+        let packet: BGPPacket = BGPPacket {
+            src: p["src"]
+                .as_str()
+                .unwrap()
+                .parse()
+                .unwrap_or("0.0.0.0".parse().unwrap()),
+            dst: p["dst"]
+                .as_str()
+                .unwrap()
+                .parse()
+                .unwrap_or("0.0.0.0".parse().unwrap()),
+            p_type: match p["type"].as_str().unwrap() {
+                "data" => BGPPacketType::Data,
+                "update" => BGPPacketType::Update,
+                "revoke" => BGPPacketType::Revoke,
+                "no route" => BGPPacketType::NoRoute,
+                "dump" => BGPPacketType::Dump,
+                "table" => BGPPacketType::Table,
+                _ => BGPPacketType::Unknown,
+            },
+            msg: p["msg"].clone(),
+        };
+        debug!("[Main Thread] Parsed Packet: {:?}", packet);
+    }
+
+    for thread in threads {
+        thread.join().expect("Error waiting for thread to exit.");
     }
 }
